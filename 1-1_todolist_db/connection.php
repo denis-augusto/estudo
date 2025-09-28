a7ed740572b77189b2812d81a389c6ae00c38cfd
Denis Augusto <denis.augusto@live.com>
2025-09-28 15:32:58 -0300

    conexão com BD e garantaindo que ao atualiazar a pagina o formulário não seja reenviado.

--- 1-1_todolist_db/connection.php
+++ 1-1_todolist_db/connection.php
@@ -3,10 +3,16 @@
     $db_file = 'database.db';
     $dsn = 'sqlite:' . $db_file;
 
+    $new_db = !file_exists($db_file);
+
     try{
         $pdo = new PDO($dsn);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+        if ($new_db){
+            $pdo->exec("CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY, todo TEXT );");
+        }
 
     } catch (PDOException $e){
         die("Erro na conexão com o Banco de dados " . $e->getMessage());
@@ -14,10 +20,20 @@
 
 // ADD TODO
 
-    $tarefa = $_POST['tarefa'];
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['tarefa'])) {
+        $tarefa = $_POST['tarefa'];
 
-    $stmt = $pdo->prepare("INSERT INTO todos (todo) VALUES ( :tarefa )");
-    $stmt->bindParam(':tarefa', $tarefa);
-    $stmt->execute();
+        try{
+            $stmt = $pdo->prepare("INSERT INTO todos (todo) VALUES (:tarefa)");
+            $stmt->bindParam(':tarefa', $tarefa);
+            $stmt->execute();
+
+            header('Location: ' . $_SERVER['PHP_SELF']);
+            exit();
+
+        } catch (PDOException $e){
+            die("Erro ao adicionar a tarefa: " . $e->getMessage());
+        }
+    };
     
 ?>
--- 1-1_todolist_db/index.php
+++ 1-1_todolist_db/index.php
@@ -20,15 +20,15 @@
         <h1 class="text-2xl text-center">Todolist com Banco de Dados (sqlite)</h1>
         <div class="border-2 rounded-md p-4">
             <form action="" method="post" class="w-full flex gap-2">
-                <input type="text" name="tarefa" id="tarefa" class="border p-1 flex-auto">
-                <input type="submit" value="Adicionar tarefa" class="border px-2 py-1 hover:bg-slate-400 hover:cursor-pointer">
+                <input type="text" name="tarefa" id="tarefa" class="border p-1 flex-auto rounded-md">
+                <input type="submit" value="Adicionar tarefa" class="border px-2 py-1 hover:bg-slate-400 hover:cursor-pointer rounded-md">
             </form>
         </div>
-        <div class="border-2 rounded-md p-4">
-            <ul>
+        <div class="border-2 rounded-md px-4 py-4">
+            <ul class="flex flex-col gap-2">
             <?php
                 foreach( $todos as $todo ){
-                    echo "<li> $todo[todo] </li></br>";
+                    echo "<li class='border-1 rounded-md px-2 py-1'> $todo[todo] </li>";
                 }
             ?>
             </ul>
