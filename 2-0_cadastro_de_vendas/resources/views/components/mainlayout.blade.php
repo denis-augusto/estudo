96b6bfedcd252168be2734c2ff843e012be19455
Denis Augusto <denis.augusto@live.com>
2025-09-30 22:56:06 -0300

    Layout blade criado

---
+++ 2-0_cadastro_de_vendas/resources/views/components/mainlayout.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>{{ $title ?? 'Título Genérico' }}</title>
+</head>
+
+<body>
+    <header
+        class="relative bg-gray-800 after:pointer-events-none after:absolute after:inset-x-0 after:inset-y-0 after:border-y after:border-white/10">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 class="text-3xl font-bold tracking-tight text-white">Estudo | 2.0 Cadastro de vendas</h1>
+            <h2 class="text-1xl tracking-tight text-white mt-2">Cadastro de vendas utilizando Laravel sem utilização de sistema de login.</h2>
+        </div>
+    </header>
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            {{ $slot }}
+        </div>
+    </main>
+</body>
+
+</html>
---
+++ 2-0_cadastro_de_vendas/resources/views/home.blade.php
@@ -0,0 +1,8 @@
+<x-mainlayout>
+    <x-slot:title>
+        Home
+    </x-slot>
+    <div>
+        Teste layout
+    </div>
+</x-mainlayout>
